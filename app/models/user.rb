5fb8445258fa50f70bab4664e68c2774df14b7b9
Jason Felds <jafelds@gmail.com>
2011-01-04 00:35:12 -0500

    Add validation to name.

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email
 
-  validates :name, :presence => true
+  validates :name, :presence => true,
+      :length => { :maximum => 50 } # magic constant, is this the best idea?
   validates :email, :presence => true
 end
