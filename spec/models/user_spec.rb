1f3d9f2badaf2b18fa45b12d107ab1e2349d4e22
Jason Felds <jafelds@gmail.com>
2011-01-04 01:11:42 -0500

    Add encrypted test, column.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -83,6 +83,16 @@ describe User do
       hash = @attr.merge(:password => long, :password_confirmation => long)
       User.new(hash).should_not be_valid
     end
+  end
 
+  describe "password encryption" do
+    
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+
+    it "should have an encrypted password attribute" do
+      @user.should respond_to(:encrypted_password)
+    end
   end
 end
